b349d890631a50c98fc6ec4cf7508e7c47602e68
engalar <engalar@gmail.com>
2024-05-08 12:50:33 +0800

    feat: combine multiple plugin

--- packages/vite-plugin-mendix/src/index.ts
+++ packages/vite-plugin-mendix/src/index.ts
@@ -1,54 +1,73 @@
-import { Connect, PluginOption } from "vite";
-import fs from "fs";
-import httpProxy from "http-proxy";
-import type { ServerResponse } from "http";
-const currentDir = __dirname;
-const proxy = httpProxy.createProxyServer({ secure: false });
+import { Connect, PluginOption } from 'vite'
+import fs from 'fs'
+import react from '@vitejs/plugin-react'
+import Inspect from 'vite-plugin-inspect'
+import httpProxy from 'http-proxy'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import type { ServerResponse } from 'http'
+
+const __filename = fileURLToPath(import.meta.url)
+const currentDir = path.dirname(__filename)
+const proxy = httpProxy.createProxyServer({ secure: false })
 const prefixs = [
-  "/__inspect",
-  "/@vite",
-  "/node_modules",
-  "/src",
-  "/@react-refresh",
-];
+  '/__inspect',
+  '/@vite',
+  '/node_modules',
+  '/src',
+  '/@react-refresh',
+]
 
 interface Options {
-  widgetName: string;
+  widgetName: string
 }
 
 export default function (opts: Options): PluginOption {
-  return {
-    name: "vite-plugin-mendix",
-    enforce: "pre",
-    load(id, options) {
-      if (id.startsWith("/src/main.js")) {
-        return fs.readFileSync(currentDir + "/../main.js", {
-          encoding: "utf-8",
-        }).replace("__WIDGET_NAME__", opts.widgetName);
-      }
-    },
-    configureServer(server) {
-      server.middlewares.use(async (req, res, next) => {
-        const url = req.url!;
-        for (const prefix of prefixs) {
-          if (url.startsWith(prefix)) {
-            next();
-            return;
+  return [
+    Inspect(),
+    react(),
+    {
+      name: 'vite-plugin-mendix',
+      enforce: 'pre',
+      load(id, options) {
+        if (id.startsWith('/src/main.js')) {
+          return fs
+            .readFileSync(currentDir + '/../main.js', {
+              encoding: 'utf-8',
+            })
+            .replace('__WIDGET_NAME__', opts.widgetName)
+        }
+      },
+      configureServer(server) {
+        server.middlewares.use(async (req, res, next) => {
+          const url = req.url!
+          for (const prefix of prefixs) {
+            if (url.startsWith(prefix)) {
+              next()
+              return
+            }
           }
-        }
-        if (url.startsWith("/mxclientsystem/mxui/mxui.js")) {
-          serveFile(req, res, "dummy.js");
-          return;
-        }
-        if (url.startsWith("/mxui.js")) {
-          serveFile(req, res, "mxui.js");
-          return;
-        }
+          if (url.startsWith('/mxclientsystem/mxui/mxui.js')) {
+            serveFile(req, res, 'dummy.js')
+            return
+          }
+          if (url.startsWith('/mxui.js')) {
+            serveFile(req, res, 'mxui.js')
+            return
+          }
 
-        proxyRequest(req, res, url);
-      });
+          proxyRequest(req, res, url)
+        })
+      },
+      buildStart(opts) {
+        console.log('buildStart')
+      },
+      buildEnd() {
+        console.log('buildEnd')
+      },
     },
-  };
+  ]
 }
 
 function proxyRequest(
@@ -56,31 +75,31 @@ function proxyRequest(
   res: ServerResponse,
   targetUrl: string,
 ): void {
-  req.url = targetUrl;
+  req.url = targetUrl
   proxy.web(req, res, {
     changeOrigin: true,
-    target: "http://localhost:8080",
-  });
-  proxy.on("error", (err) => {
-    res.statusCode = 500;
-    res.end(err.message);
-  });
+    target: 'http://localhost:8080',
+  })
+  proxy.on('error', (err) => {
+    res.statusCode = 500
+    res.end(err.message)
+  })
 }
 function serveFile(
   req: Connect.IncomingMessage,
   res: ServerResponse,
   filePath: string,
 ): void {
-  filePath = currentDir + "/../" + filePath;
+  filePath = currentDir + '/../' + filePath
   fs.readFile(filePath, (err, data) => {
     if (err) {
       // 发生错误时返回错误响应
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Internal Server Error");
-      return;
+      res.writeHead(500, { 'Content-Type': 'text/plain' })
+      res.end('Internal Server Error')
+      return
     }
     // 返回文件内容作为响应
-    res.writeHead(200, { "Content-Type": "text/javascript" });
-    res.end(data);
-  });
+    res.writeHead(200, { 'Content-Type': 'text/javascript' })
+    res.end(data)
+  })
 }
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,26 +1,26 @@
-import { release } from "@vitejs/release-scripts";
-import colors from "picocolors";
-import { logRecentCommits, run } from "./releaseUtils";
+import { release } from '@vitejs/release-scripts'
+import colors from 'picocolors'
+import { logRecentCommits, run } from './releaseUtils'
 
 release({
-  repo: "vite-plugin-monorepo",
-  packages: ["vite-plugin-mendix"],
+  repo: 'vite-plugin-monorepo',
+  packages: ['vite-plugin-mendix'],
   toTag: (pkg, version) => `${pkg}@${version}`,
   logChangelog: (pkg) => logRecentCommits(pkg),
   generateChangelog: async (pkgName) => {
-    console.log(colors.cyan("\nGenerating changelog..."));
+    console.log(colors.cyan('\nGenerating changelog...'))
     const changelogArgs = [
-      "conventional-changelog",
-      "-p",
-      "angular",
-      "-i",
-      "CHANGELOG.md",
-      "-s",
-      "--commit-path",
-      ".",
-      "--lerna-package",
+      'conventional-changelog',
+      '-p',
+      'angular',
+      '-i',
+      'CHANGELOG.md',
+      '-s',
+      '--commit-path',
+      '.',
+      '--lerna-package',
       pkgName,
-    ];
-    await run("npx", changelogArgs, { cwd: `packages/${pkgName}` });
+    ]
+    await run('npx', changelogArgs, { cwd: `packages/${pkgName}` })
   },
-});
+})
