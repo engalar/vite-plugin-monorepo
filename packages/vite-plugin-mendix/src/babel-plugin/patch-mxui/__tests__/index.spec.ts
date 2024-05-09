6dec809fe2ee467cfd52a58ea641f80d07b39169
engalar <engalar@gmail.com>
2024-05-09 13:33:59 +0800

    chore: code framework

--- packages/vite-plugin-mendix/src/babel-plugin/patch-mxui/__tests__/index.spec.ts
+++ packages/vite-plugin-mendix/src/babel-plugin/patch-mxui/__tests__/index.spec.ts
@@ -2,7 +2,9 @@ import { describe, expect, test } from 'vitest'
 import { babelPluginPatchMxui } from '..'
 
 describe('babelPluginPatchMxui', () => {
-  test('adds 1 + 2 to equal 5', () => {
-    expect(babelPluginPatchMxui()).toBeUndefined()
+  test('adds 1 + 2 to equal 5', async () => {
+    expect(await babelPluginPatchMxui('var a = () => {};')).toBe(
+      'var a = function () {};',
+    )
   })
 })
